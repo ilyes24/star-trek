99bd4194b10ef6ab80245fbb80ebd6abf489a14c
ilyes24 <ilyes24ghomrani@gmail.com>
2020-10-21 16:54:17 +0100

    ensure polufills work correctly

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import './polyfills.ts'; // recommended by heroku - To ensure that polyfills work correctly
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
