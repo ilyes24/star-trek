a63cfc8896bb3859e9d8816c38606c19bb27882d
ilyes24 <ilyes24ghomrani@gmail.com>
2020-10-22 07:44:22 +0100

    bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EpisodeComponent } from './episode/episode.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MenuComponent },
+  { path: 'episode/:id', component: EpisodeComponent },
+  { path: '**', component: MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
